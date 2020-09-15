de1bf9e77abae08e67bc9885f73220d1014b078a
engrbugs <engrbugs@gmail.com>
2020-09-14 22:29:33 -0700

    2020-09-14-2229

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-## See this in action: [https://engrbugs.github.io/D3BarChart/](https://engrbugs.github.io/D3BarChart/)
+## See this in action: [https://engrbugs.github.io/D3ScatterPlot/](https://engrbugs.github.io/D3ScatterPlot/)
 <br>
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const tooltip = document.getElementById('tooltip');
 
-fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json')
     .then(res => res.json())
     .then(res => {
-        const { data } = res;
-        createGraph(data);
+        createGraph(res.map( r => [new Date(1970, 0, 1, 0, r.Time.split(':')[0], r.Time.split(':')[1]), r.Year]));
     })
 
 function createGraph(data) {
+    console.log(data);
     var width = $(window).width() - 480,
         height = $(window).height() - 280,
         xPadding = 60,
@@ -15,14 +15,13 @@ function createGraph(data) {
         xyrPadding = 40,
         barWidth = (width) / (data.length);
 
-    var xMax = new Date(d3.max(data, d => new Date(d[0])));
-    xMax.setMonth(xMax.getMonth() + 3);
 
-    var xScale = d3.scaleTime()
-        .domain([d3.min(data, d => new Date(d[0])), xMax])
+
+    var xScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d[1] - 1), d3.max(data, d => d[1] + 1)])
         .range([xPadding, width + xPadding]) 
 
-    var yScale = d3.scaleLinear()
+    var yScale = d3.scaleT()
         .domain([0, d3.max(data, d => d[1])])
         .range([height + yPadding, yPadding]);
     
@@ -30,45 +29,45 @@ function createGraph(data) {
         .attr('width', width + xPadding + xyrPadding)
         .attr('height', height + yPadding + xyrPadding);
     
-    svg.selectAll('rect')
-        .data(data)
-        .enter()
-        .append('rect')
-        .attr('class', 'bar')
-        .attr('data-date', d => d[0])
-        .attr('data-gdp',d => d[1])
-        .attr('x', (d, i) => i * barWidth + xPadding)
-        .attr('y', d => yScale(d[1]) - yPadding + xyrPadding)
-        .attr('width', barWidth)
-        .attr('height', d => height - yScale(d[1]) + yPadding + 'px')
-        .on('mousemove',  (d, item) => {
-            let yearLine = "";
-            switch(item[0].substring(5, 7)) {
-                case '01':
-                    yearLine = item[0].substring(0, 4) + ' Q1';
-                    break;
-                case '04':
-                    yearLine = item[0].substring(0, 4) + ' Q2';
-                    break;
-                case '07':
-                    yearLine = item[0].substring(0, 4) + ' Q3';
-                    break;
-                case '10':
-                    yearLine = item[0].substring(0, 4) + ' Q4';
-                    break;
-                default:
-                  // code block
-              };
+    // svg.selectAll('rect')
+    //     .data(data)
+    //     .enter()
+    //     .append('rect')
+    //     .attr('class', 'bar')
+    //     .attr('data-date', d => d[0])
+    //     .attr('data-gdp',d => d[1])
+    //     .attr('x', (d, i) => i * barWidth + xPadding)
+    //     .attr('y', d => yScale(d[1]) - yPadding + xyrPadding)
+    //     .attr('width', barWidth)
+    //     .attr('height', d => height - yScale(d[1]) + yPadding + 'px')
+    //     .on('mousemove',  (d, item) => {
+    //         let yearLine = "";
+    //         switch(item[0].substring(5, 7)) {
+    //             case '01':
+    //                 yearLine = item[0].substring(0, 4) + ' Q1';
+    //                 break;
+    //             case '04':
+    //                 yearLine = item[0].substring(0, 4) + ' Q2';
+    //                 break;
+    //             case '07':
+    //                 yearLine = item[0].substring(0, 4) + ' Q3';
+    //                 break;
+    //             case '10':
+    //                 yearLine = item[0].substring(0, 4) + ' Q4';
+    //                 break;
+    //             default:
+    //               // code block
+    //           };
 
-            let gdpLine = "$ " + item[1].toFixed(1).replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' Billion' 
+    //         let gdpLine = "$ " + item[1].toFixed(1).replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' Billion' 
 
-            tooltip.style.left = d.pageX + xyrPadding + 'px';
-            tooltip.style.top = height + xyrPadding + 'px';
-            tooltip.innerHTML = yearLine + "<br/>" + gdpLine;
-            tooltip.setAttribute("data-date", item[0]);
-        })
-        .on('mouseover', () => tooltip.style.visibility = "visible")
-        .on('mouseout', () => tooltip.style.visibility = "hidden")
+    //         tooltip.style.left = d.pageX + xyrPadding + 'px';
+    //         tooltip.style.top = height + xyrPadding + 'px';
+    //         tooltip.innerHTML = yearLine + "<br/>" + gdpLine;
+    //         tooltip.setAttribute("data-date", item[0]);
+    //     })
+    //     .on('mouseover', () => tooltip.style.visibility = "visible")
+    //     .on('mouseout', () => tooltip.style.visibility = "hidden")
 
 
 
